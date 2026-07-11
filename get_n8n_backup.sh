5a2df14880a3e056a6c75052b9f938849c6b36a1
gabrielms-castro <gabrielms.castro@gmail.com>
2026-07-10 23:27:45 -0300

    fix: update directory path in n8n backup script

--- get_n8n_backup.sh
+++ get_n8n_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker exec octobot-n8n sh -lc '
-  cd /home/node/.n8n &&
+  cd /home/node &&
   wget -O backup.tar "https://octo-backups-asdf98.s3.us-east-1.amazonaws.com/n8n_volume_backup.tar" &&
   tar -xf backup.tar &&
   rm backup.tar
