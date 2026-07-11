43156329f078a01f343648d0e5c61520ba5f818d
gabrielms-castro <gabrielms.castro@gmail.com>
2026-07-10 23:16:15 -0300

    fix: correct backup file naming in n8n backup script

--- get_n8n_backup.sh
+++ get_n8n_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker exec octobot-n8n sh -lc '
   cd /home/node/.n8n &&
-  wget -O backup.tar.gz "https://octo-backups-asdf98.s3.us-east-1.amazonaws.com/n8n_volume_backup.tar" &&
-  tar -xf backup.tar.gz &&
-  rm backup.tar.gz
+  wget -O backup.tar "https://octo-backups-asdf98.s3.us-east-1.amazonaws.com/n8n_volume_backup.tar" &&
+  tar -xf backup.tar &&
+  rm backup.tar
 '
